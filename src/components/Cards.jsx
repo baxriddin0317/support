43ee2ac6ee49a6baa6291a3f71249a21e23df417
AbdushukurRasulov <abdushukurrasulov1994@gmail.com>
2023-05-13 23:21:24 +0500

    updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -10,24 +10,24 @@ const cardsData = [
   {
     id: 2,
     imgName: 'account',
-    title: 'google account',
+    title: 'Google account',
   },
   {
     id: 3,
     imgName: 'youtube',
-    title: 'youTube',
+    title: 'YouTube',
   },
   {
     id: 4,
     imgName: 'gmail',
-    title: 'gmail',
+    title: 'Gmail',
   },
 ];
 
 const Cards = () => {
   return (
     <section className='max-w-4xl py-20 mx-auto px-5'>
-      <div className='max-w-3xl mx-auto space-y-6 mb-24'>
+      <div className='max-w-3xl mx-auto space-y-6 mb-16'>
         {cardsData.map((data) => (
           <Card key={data.id} data={data} />
         ))}
@@ -46,6 +46,15 @@ const Cards = () => {
           </svg>
         </div>
       </div>
+
+      <div className='max-w-3xl mx-auto px-5 text-center mt-12'>
+        <h3 className='text-2xl'>Lorem ipsum dolor sit amet</h3>
+        <p className='mt-3'>
+          consectetur adipisicing elit. Enim dignissimos distinctio rerum
+          asperiores temporibus aut quia nam accusamus reiciendis perspiciatis.
+          Quos corporis iste laudantium vero itaque culpa eos
+        </p>
+      </div>
     </section>
   );
 };
@@ -55,28 +64,24 @@ export default Cards;
 const Card = ({ data }) => {
   return (
     <div className='border border-gray-300 rounded-lg hover:shadow-lg transition-shadow duration-200 py-5 px-8'>
-      <div className='flex items-center space-x-4'>
+      <div className='flex items-start space-x-4'>
         <div className='max-w-[4rem] shrink-0'>
           <img
-            className='w-full h-full object-contain'
+            className='w-full object-contain'
             src={require(`../assets/${data.imgName}.png`)}
             alt={data.title}
           />
         </div>
 
-        <div className='text-blue-800 '>
+        <div className='flex-1 text-blue-800 '>
           <h3 className='text-xl font-medium'>{data.title}</h3>
           <p className='mt-1'>This is more infomation</p>
-        </div>
-      </div>
 
-      <div className='flex items-center justify-between px-4 mt-8'>
-        <Link to='/' className='text-center text-blue-800'>
-          Link to learn more
-        </Link>
-        <Link to='/' className='text-center text-blue-800'>
-          Link to schedule more
-        </Link>
+          <div className='flex items-center justify-between mt-8'>
+            <Link to='/'>Link to learn more</Link>
+            <Link to='/'>Link to schedule more</Link>
+          </div>
+        </div>
       </div>
     </div>
   );
