9fd5c8b28cafc8223f9a489938fa7298b175a6f1
AbdushukurRasulov <abdushukurrasulov1994@gmail.com>
2023-05-16 23:56:51 +0500

    responsive fix

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
       <main className='max-w-7xl px-5 md:px-10 mx-auto'>
         {/* hero */}
         <section className='relative py-10'>
-          <div className='max-w-2xl mx-auto text-center'>
+          <div className='max-w-xl xl:max-w-2xl mx-auto text-center'>
             <h1 className='text-2xl md:text-4xl  text-blue-800 font-semibold mt-4'>
               How can we help <span>you</span>
             </h1>
-            <p className='text-gray-500 mt-5'>
+            <p className='text-gray-500 text-sm lg:text-base mt-5'>
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s
